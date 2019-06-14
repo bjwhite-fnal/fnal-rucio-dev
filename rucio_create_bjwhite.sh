5aec1b108b6957c62354fbccbcfadf093149eca2
Brandon White <bjwhite@fnal.gov>
2019-06-14 11:12:40 -0500

    Development environment now bootstraps only X509 authentication using my (bjwhte) Fermilab InCommon X509 certificate.

---
+++ rucio_create_bjwhite.sh
@@ -0,0 +1,2 @@
+rucio-admin account add bjwhite
+rucio-admin identity add --account bjwhite --type X509 --id "/DC=org/DC=cilogon/C=US/O=Fermi National Accelerator Laboratory/OU=People/CN=Brandon White/CN=UID:bjwhite" --email bjwhite@fnal.gov
--- rucio_init_db.sh
+++ rucio_init_db.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#./wait-for-it.sh -h mysql -p 3306 -t 90
 ./wait-for-it.sh -h postgres -p 5432 -t 90
 ./wait-for-it.sh -h rucio-dev -p 443 -t 90
-python -c "import rucio.db.sqla.util as rdb; rdb.build_database(); rdb.create_root_account();"
+python -c "import util as rdb; rdb.build_database(); rdb.create_root_account();"
